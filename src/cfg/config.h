c7abd07a3a58308da90c51a4e883d76ad3b529cd
Nicole Holloway <jessinthehallway@gmail.com>
2024-09-29 18:37:42 +0800

    Everything goes in a Linux deploy.

---
+++ src/cfg/config.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef config_h
+#define config_h
+
+#include <string>
+#include <dpp/dpp.h>
+
+extern std::string bot_token;
+extern std::string prefix;
+extern std::string taunt_prefix;
+extern dpp::snowflake owner_id;
+
+#endif
