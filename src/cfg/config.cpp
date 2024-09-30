6187b8a9ef35fc492dbaedce678590c3ce6fe8ae
Nicole Holloway <jessinthehallway@gmail.com>
2024-10-01 00:12:59 +0800

    Patch out token

--- src/cfg/config.cpp
+++ src/cfg/config.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <dpp/dpp.h>
 
-std::string bot_token = "OTQ4NzcxMDUzMzk0NTk1OTUx.GwXfM5.USGYn2y76kZkZTsoen-AqgEkKDtDUdCa0shr-k";
+std::string bot_token = "no-token.you-must-get-one-yourself";
 std::string prefix = "> ";
 std::string taunt_prefix = "_";
 dpp::snowflake owner_id = 785469781163245569;
